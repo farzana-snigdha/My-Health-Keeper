10ed65feef9c794ada689c3d5bc686980b63a6a3
SabrinaSusmi <sabrina.su512@gmail.com>
2021-06-21 22:40:00 +0600

    router linked in index.js

--- server/index.js
+++ server/index.js
@@ -1,22 +1,26 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
-const dotenv = require('dotenv')
-
-dotenv.config()
-
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
 const app = express();
+
+const routeURLs = require('./routes/route');
+
+dotenv.config();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+//app.use(express.json())
 app.use(cors());
 
 //const CONNECTION_URL = 'mongodb+srv://sabrina_susmi:sus123@cluster0.pyyns.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT;
 
 mongoose.connect(process.env.DATABASE_ACCESS, () =>console.log('DB connected'),  { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`)))
-  .catch((error) => console.log(error.message));
+.catch((error) => console.log(error.message));
 
 mongoose.set('useFindAndModify', false);
+
+app.use('/app', routeURLs);
+app.listen(PORT, () => console.log(`Server is running at PORT ${PORT}.`))
