63f243fcb4ffbd0e818bb7711946914f83f6ca0c
Siana Rizwan <sianarizwan@gmail.com>
2021-06-26 00:43:06 +0600

    Creating Ui for displaying medicine reminders list

---
+++ frontend/src/components/body/medicineReminder/DisplayMedReminders.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import './medicineReminder.css'
+
+function displayMedicineReminders() {
+    return (
+           
+          <div className="reminder">
+              <h1>Reminder List 🧭</h1>
+        <div className="reminder_body">
+          {
+            
+              <div className="reminder_card" >
+                <h2>HEHEHEHHEH</h2>
+                <h3>Work should be done by:</h3>
+                <p>uhuisgdufegforgp4igigbh</p>
+                <div className="button" >Delete</div>
+              </div>
+              
+            
+          }
+          {
+            
+            <div className="reminder_card" >
+              <h2>HEHEHEHHEH</h2>
+              <h3>Work should be done by:</h3>
+              <p>uhuisgdufegforgp4igigbh</p>
+              <div className="button" >Delete</div>
+            </div>
+            
+          
+        }
+        {
+            
+            <div className="reminder_card" >
+              <h2>HEHEHEHHEH</h2>
+              <h3>Work should be done by:</h3>
+              <p>uhuisgdufegforgp4igigbh</p>
+              <div className="button" >Delete</div>
+            </div>
+            
+          
+        }
+          
+
+          
+        </div>
+
+      </div>
+    
+    );
+}
+
+export default displayMedicineReminders;
