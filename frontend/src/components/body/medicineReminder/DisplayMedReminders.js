8e3887bb91182fd6514dc28e8daf74aba84a6126
Siana Rizwan <sianarizwan@gmail.com>
2021-06-26 01:41:08 +0600

    trying to change  components behaviour

--- frontend/src/components/body/medicineReminder/DisplayMedReminders.js
+++ frontend/src/components/body/medicineReminder/DisplayMedReminders.js
@@ -1,20 +1,28 @@
 import React from 'react'
 import './medicineReminder.css';
+import {Switch, Route} from 'react-router-dom'
 import { IconButton } from "@material-ui/core";
 import DeleteIcon from '@material-ui/icons/Delete';
 import red from "@material-ui/core/colors/red";
+import InputMed from './InputMedReminder';
 
 function displayMedicineReminders() {
+    const addReminder = () => {
+        
+    }
     return (
            
           <div className="reminder">
+              <div className="reminder_header">
               <h1>Reminder List 🧭</h1>
+              <div className="button" onClick={addReminder}>Add To List</div>
+              </div>
         <div className="reminder_body">
           {
             
               <div className="reminder_card" >
                 <h2>MEDICINE NAME</h2>
-                <h3>Description</h3>
+                <p>Description</p>
                 <p>Time:  </p>
                 <IconButton className="btn" >
                 <DeleteIcon style={{ color: "red" }} />
@@ -26,26 +34,26 @@ function displayMedicineReminders() {
           {
             
             <div className="reminder_card" >
-            <h2>MEDICINE NAME</h2>
-            <h3>Description</h3>
-            <p>Time:  </p>
-            <IconButton className="btn" >
-            <DeleteIcon style={{ color: "red" }} />
-           </IconButton>
-          </div>
+                <h2>MEDICINE NAME</h2>
+                <p>Description</p>
+                <p>Time:  </p>
+                <IconButton className="btn" >
+                <DeleteIcon style={{ color: "red" }} />
+               </IconButton>
+              </div>
             
           
         }
         {
             
             <div className="reminder_card" >
-                <h2>MEDICINE NAME</h2>
-                <h3>Description</h3>
-                <p>Time:  </p>
-                <IconButton className="btn" >
-                <DeleteIcon style={{ color: "red" }} />
-               </IconButton>
-              </div>
+            <h2>MEDICINE NAME</h2>
+            <p>Description</p>
+            <p>Time:  </p>
+            <IconButton className="btn" >
+            <DeleteIcon style={{ color: "red" }} />
+           </IconButton>
+          </div>
             
           
         }
