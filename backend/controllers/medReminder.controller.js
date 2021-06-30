af0599f17d70c75954bbff1d4b536393208a2451
SabrinaSusmi <sabrina.su512@gmail.com>
2021-06-30 13:33:56 +0600

    displaying med in dash created

--- backend/controllers/medReminder.controller.js
+++ backend/controllers/medReminder.controller.js
@@ -1,12 +1,20 @@
 const medReminder = require('../models/medReminder.model')
 
 
-const getMedicine = (req,res) => {
-
+const getMedicine = async (req,res) => {
+     medReminder.find({}, (err,reminderList)=> {
+         if(err){
+             consol.log(err)
+         }
+         if(reminderList){
+             res.send(reminderList)
+         }
+     })
 }
 
-const postMedicine = async (req, res) => {
+const postMedicine =async (req, res) => {
     let user = req.user.id;
+
     const {username, medname, descriptionmed,startdate, enddate, doses} = req.body;
 
     const med = new medReminder ({
---
+++ frontend/src/components/body/medicineReminder/DisplayMedReminders.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import './medicineReminder.css';
+import {Switch, Route} from 'react-router-dom'
+import { useState, useEffect } from "react"
+import axios from "axios"
+import { IconButton,Link } from "@material-ui/core";
+import DeleteIcon from '@material-ui/icons/Delete';
+import red from "@material-ui/core/colors/red";
+
+
+function displayMedicineReminders() {
+  const [ medname, setMedName ] = useState("")
+  const [ descriptionmed, setDescriptionMed] =  useState("")
+  const [ time, setRemindAt ] = useState([])
+  const [ reminderList, setReminderList ] = useState([])
+
+  useEffect(() => {
+      axios.get("http://localhost:5000/medReminder").then( res => setReminderList(res.data))
+  }, [])
+
+    return (
+           
+          <div className="reminder">
+              <div className="reminder_header">
+              <h1>Reminder List 🧭</h1>
+              <Link href= "/medicine-reminder"  className="button" > Add to List</Link>
+              </div>
+        <div className="reminder_body">
+          {
+            reminderList.map(medicines=>(
+              <div className="reminder_card" >
+                <h2>{medicines.medName}</h2>
+                <p>{medicines.descriptionmed}</p>
+                <p>Time:  </p>
+                <IconButton className="btn" >
+                <DeleteIcon style={{ color: "red" }} />
+               </IconButton>
+              </div>
+            ))
+            
+          }
+        
+          
+
+          
+        </div>
+
+      </div>
+    
+    );
+}
+
+export default displayMedicineReminders;
