e3f53c25bf6b00057a7e27b9494f9b0e6406e4d5
SabrinaSusmi <sabrina.su512@gmail.com>
2021-07-03 21:40:21 +0600

    finally dynamically reminder cards are displayed

--- frontend/src/components/body/medicineReminder/DisplayMed.js
+++ frontend/src/components/body/medicineReminder/DisplayMed.js
@@ -3,28 +3,44 @@ import "./medicineReminder.css"
 import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import axios from "axios";
+import { IconButton, Link } from "@material-ui/core";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 function DisplayMed () {
     const token = useSelector((state) => state.token);
     const [ reminderList, setReminderList ] = useState([]);
 
-    useEffect(() => {
-        axios.get("/medReminder", {
+    useEffect(async () => {
+       await axios.get("/medReminder", {
             headers: { Authorization: token },
           }).then( res => setReminderList(res.data))
     }, [])
 
     return(
+        <div className="reminder">
+      <div className="reminder_header">
+        <h1>Reminder List 🧭</h1>
+        <Link href="/medicine-reminder" className="button">
+          {" "}
+          Add to List
+        </Link>
+      </div>
+
         <div className="reminder_body">
           {
             reminderList.map(medicines=>(
               <div className="reminder_card" >
                 <h2>{medicines.medname}</h2>
                 <p>{medicines.descriptionmed}</p>
-                <p>Time:  </p>
+                <p>{medicines.startdate.substring(0, 10)}</p>
+                <p>{medicines.enddate.substring(0, 10)}</p>
+                <IconButton className="btn" >
+                <DeleteIcon style={{ color: "red" }} />
+               </IconButton>
               </div>
             ))
           }
+        </div>
         </div>
     )
 }
