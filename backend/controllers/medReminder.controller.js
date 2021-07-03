d2e6769e60166e98c73c22afee528d667242ae35
SabrinaSusmi <sabrina.su512@gmail.com>
2021-07-04 02:15:22 +0600

    delete reminder - Invalid authentication

--- backend/controllers/medReminder.controller.js
+++ backend/controllers/medReminder.controller.js
@@ -1,50 +1,60 @@
-const medReminder = require('../models/medReminder.model')
+const medReminder = require("../models/medReminder.model");
 
+const getMedicine = async (req, res) => {
+  let user = req.user.id;
 
-const getMedicine = async (req,res) => {
-    let user = req.user.id;
-
-    medReminder.find({user}, (err,reminderList)=> {
-        if(err){
-            console.log(user)
-            console.log("Test :" + err)
-        }
-        if(reminderList){
-            res.send(reminderList)
-            console.log(reminderList);
-        }
-    })
-}
-
-const postMedicine =async (req, res) => {
-    let user = req.user.id;
-
-    const {username, medname, descriptionmed,startdate, enddate, doses} = req.body;
-
-    const med = new medReminder ({
-        user,
-        username : username,
-        medname : medname,
-        descriptionmed : descriptionmed,
-        startdate : startdate,
-        enddate : enddate,
-        time : doses
-    })
-    med.save()
-    .then(data => {
-        res.json(data);
-        console.log(data);
-    })
-    .catch(error => {
-        res.json(error)
-    })
+  medReminder.find({ user }, (err, reminderList) => {
+    if (err) {
+      console.log(user);
+      console.log("Test :" + err);
+    }
+    if (reminderList) {
+      res.send(reminderList);
+      console.log(reminderList);
+    }
+  });
 };
 
-const deleteMedicine = (req,res) => {
-    let user = req.user.id;
-    medReminder.findByIdAndDelete({user}, req.params.id)
-    .then(() => res.json('Reminder deleted.'))
-    .catch(err => res.status(400).json('Error: ' + err));
-}
+const postMedicine = async (req, res) => {
+  let user = req.user.id;
+
+  const { username, medname, descriptionmed, startdate, enddate, doses } =
+    req.body;
+
+  const med = new medReminder({
+    user,
+    username: username,
+    medname: medname,
+    descriptionmed: descriptionmed,
+    startdate: startdate,
+    enddate: enddate,
+    time: doses,
+  });
+  med
+    .save()
+    .then((data) => {
+      res.json(data);
+      console.log(data);
+    })
+    .catch((error) => {
+      res.json(error);
+    });
+};
+
+const deleteMedicine = async (req, res) => {
+  let user = req.user.id;
+  medReminder.find({ user }, (err, reminderList) => {
+    if (err) {
+      console.log(user);
+      console.log("Test :" + err);
+    }
+    if (reminderList) {
+      medReminder
+        .findByIdAndDelete(req.data.medId)
+        .then(() => res.json("Reminder deleted."))
+        .catch((err) => res.status(400).json("Error: " + err));
+    }
+  });
+};
 
 module.exports = { getMedicine, postMedicine, deleteMedicine };
