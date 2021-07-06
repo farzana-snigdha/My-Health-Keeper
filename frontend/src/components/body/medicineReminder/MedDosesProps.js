ca1f12462984412d9ee4abdb5ef2f3340fbed6a4
SabrinaSusmi <sabrina.su512@gmail.com>
2021-07-06 23:03:27 +0600

    Props added

---
+++ frontend/src/components/body/medicineReminder/MedDosesProps.js
@@ -0,0 +1,55 @@
+import React from "react";
+import "../../../static/Styling/medicineReminder.css";
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import axios from "axios";
+import { Button, IconButton, Link } from "@material-ui/core";
+
+const DailyDoses = (props) => {
+    return (
+        <div className="reminder_card">
+          <h2>{props.doses.medname}</h2>
+          <p>Time: {props.doses.medtime}</p>
+          <p>Taken: {props.doses.isTaken}</p>
+        </div>
+    )
+}
+
+function MedDosesProps () {
+    const token = useSelector((state) => state.token);
+  const auth = useSelector((state) => state.auth);
+  const { user } = auth;
+  const [doseList, setDoseList] = useState([]);
+  
+  useEffect(async () => {
+    await axios
+      .get("/medDose", {
+        headers: { Authorization: token },
+      })
+      .then((res) => setDoseList(res.data));
+  }, []);
+
+  const confirmReminder = async (id) => {
+    await axios.post('/medDose/'+id,
+    {
+      headers: { Authorization: token, userId : user._id },
+    })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div>
+      {doseList.map((currentdose) => (
+        <DailyDoses doses={currentdose} />
+      ))}
+    </div>
+  );
+
+}
+
+export default MedDosesProps;
