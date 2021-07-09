019050eb14dce69c5588e9dc459df08308d6b2ea
SabrinaSusmi <sabrina.su512@gmail.com>
2021-07-09 14:23:16 +0600

    modal changed

--- frontend/src/components/body/medicineReminder/MedModal.js
+++ frontend/src/components/body/medicineReminder/MedModal.js
@@ -1,16 +1,27 @@
 import React, { useRef, useEffect, useCallback } from 'react';
-import "../../../static/Styling/addNotesModal.css";
 import styled from 'styled-components';
 import { MdClose } from 'react-icons/md';
 
+const Background = styled.div`
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.6);
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const ModalWrapper = styled.div`
   width: 30%;
-  height: 50%;
+  height: 40%;
   box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
   background: #fff;
   color: #000;
-  display: grid;
   position: relative;
   border-radius: 10px;
 `;
@@ -20,7 +31,6 @@ const ModalContent = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  line-height: 1.8;
   color: #141414;
   p {
     margin-bottom: 1rem;
@@ -45,49 +55,52 @@ const CloseModalButton = styled(MdClose)`
 `;
 
 const MedModal = ({ showModal, setShowModal }) => {
-    const modalRef = useRef();
-  
-    const closeModal = e => {
-      if (modalRef.current === e.target) {
-        setShowModal(false);
-      }
-    };
-  
-    const keyPress = useCallback(
-      e => {
-        if (e.key === 'Escape' && showModal) {
-          setShowModal(false);
-          console.log('I pressed');
-        }
-      },
-      [setShowModal, showModal]
-    );
-  
-    useEffect(
-      () => {
-        document.addEventListener('keydown', keyPress);
-        return () => document.removeEventListener('keydown', keyPress);
-      },
-      [keyPress]
-    );
-  
-    return (
-      <>
-        {showModal ? (
-            <div className="modal" onClick={closeModal} ref={modalRef}>
-              <ModalWrapper showModal={showModal}>
-                <ModalContent>
-                  
-                </ModalContent>
-                <CloseModalButton
-                  aria-label='Close modal'
-                  onClick={() => setShowModal(prev => !prev)}
-                />
-              </ModalWrapper>
-          </div>
-        ) : null}
-      </>
-    );
+  const modalRef = useRef();
+
+
+  const closeModal = e => {
+    if (modalRef.current === e.target) {
+      setShowModal(false);
+    }
   };
 
-  export default MedModal;
+  const keyPress = useCallback(
+    e => {
+      if (e.key === 'Escape' && showModal) {
+        setShowModal(false);
+        console.log('I pressed');
+      }
+    },
+    [setShowModal, showModal]
+  );
+
+  useEffect(
+    () => {
+      document.addEventListener('keydown', keyPress);
+      return () => document.removeEventListener('keydown', keyPress);
+    },
+    [keyPress]
+  );
+
+  return (
+    <>
+      {showModal ? (
+        <Background onClick={closeModal} ref={modalRef}>
+          
+            <ModalWrapper showModal={showModal}>
+              <ModalContent>
+                
+              </ModalContent>
+              <CloseModalButton
+                aria-label='Close modal'
+                onClick={() => setShowModal(prev => !prev)}
+              />
+            </ModalWrapper>
+          
+        </Background>
+      ) : null}
+    </>
+  );
+};
+
+export default MedModal;
