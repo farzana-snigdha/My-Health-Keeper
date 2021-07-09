242d2635072a8eaa286899c7cb2fc80f9ab4d77e
SabrinaSusmi <sabrina.su512@gmail.com>
2021-07-09 19:25:47 +0600

    controller and router for genHealth

---
+++ backend/controllers/genHealthController.js
@@ -0,0 +1,10 @@
+const genHealthSchema = require('../models/genHealth.model');
+
+const postHealthInfo = async (req,res) => {
+    let user = req.user.id;
+
+    
+
+}
+
+module.exports = { postHealthInfo };
