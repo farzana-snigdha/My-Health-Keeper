e852c74860266a03c8d2b7edc4237987b06443a7
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2021-07-09 22:10:22 +0600

    general health backend added

---
+++ backend/models/genHealth.model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const genHealthSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+      },
+    infoTitle: {
+        type: String,
+        required: true
+    },
+    info: {
+        type: Number,
+        required: true
+    },
+    inputDate: {
+        type : Date,
+        default : Date.now
+    }
+})
+
+module.exports = mongoose.model('genHealth', genHealthSchema)
