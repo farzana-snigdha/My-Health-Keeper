3056cfa958ba0946611dd02408be40c5b2015a20
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2021-07-17 12:08:58 +0600

    view folder added, tried to solve refresh problem, useContext added

--- frontend/src/components/body/specializedHealthInfo/specializedHealthInfo.js
+++ frontend/src/components/body/specializedHealthInfo/specializedHealthInfo.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useContext, useRef } from "react";
 import "../../../static/Styling/spHealthInfo.css";
 import { useSelector } from "react-redux";
 import VisibilityIcon from "@material-ui/icons/Visibility";
@@ -8,7 +8,7 @@ import FolderSpecialIcon from "@material-ui/icons/FolderSpecial";
 import { useHistory } from "react-router-dom";
 import axios from "axios";
 import AddNotes from "./AddNotes";
-
+import { UserIDContext } from "../../../App";
 const initialState = {
   folder: "",
   noteDate: "",
@@ -16,25 +16,32 @@ const initialState = {
 };
 
 export default function SpecializedHealthInfo() {
+  const userID = useContext(UserIDContext);
   const token = useSelector((state) => state.token);
   const auth = useSelector((state) => state.auth);
   const { user, isLogged } = auth;
 
+  console.log("cdcwdcwc ", userID);
   let history = useHistory();
   const [spHealthNotes, setSpHealthNotes] = useState([]);
 
   const showSPHealthNotes = async () => {
-    const id = user._id;
-
-    await axios
-      .get("http://localhost:5000/api/get-specializedHealthInfo", {
-        headers: { Authorization: token, userid: id },
-      })
-      .then((res) => setSpHealthNotes(res.data));
+    let oo = localStorage.getItem("spUser");
+    if (oo) {
+      console.log("sp id     ", oo);
+      await axios
+        .get("http://localhost:5000/api/get-specializedHealthInfo", {
+          headers: { Authorization: token, userid: userID },
+        })
+        .then((res) => {
+          console.log(res.data);
+          history.push("/specialized-health-information");
+          setSpHealthNotes(res.data);
+        });
+    }
   };
 
-  useEffect(async () => {
-    localStorage.getItem("setSpHealthNotes");
+  useEffect(() => {
     showSPHealthNotes();
   }, []);
 
@@ -44,41 +51,56 @@ export default function SpecializedHealthInfo() {
         headers: { Authorization: token, folderid: folderId },
       })
       .then((res) => {
-        // history.push({pathname:`/view-files?id=${folderId}`,
-        //  state:folderId });
-        //  return folderId
+        history.push({ pathname: `/view-files`, state: folderId });
+        return folderId;
       });
   };
   return (
     <div>
-      {AddNotes()}
+      <AddNotes getNote={() => showSPHealthNotes()} />
       <h4>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Health Diary</h4>
       <hr></hr>
-      <div>
-        {spHealthNotes.map((note) => (
-          <div variant="outlined" className="reminder_card">
-            <h2>
-              {" "}
-              <FolderSpecialIcon />
-              &nbsp;{note.folder}
-            </h2>
-            <hr></hr>
-            <p>Note Date: {note.noteDate.substring(0, 10)}</p>
-            <p>Description: {note.description}</p>
-            <IconButton
-              component={Link}
-              to={{state: note._id, pathname: `/view-files?id=${note._id}` }}
-              className="viewBtn"
-              data-toggle="tooltip"
-              title="View Your Saved Files"
-              onClick={() => viewFolder(note._id)}
-            >
-              <VisibilityIcon />
-            </IconButton>
-            {/* </Link> */}
-          </div>
-        ))}
-      </div>
+
+      {console.log("spHealthNotes.length", spHealthNotes)}
+
+      {spHealthNotes.length != 0 ? (
+        <div>
+          {spHealthNotes.map((note) => (
+            <div variant="outlined" className="reminder_card">
+              <h2>
+                {" "}
+                <FolderSpecialIcon />
+                &nbsp;{note.folder}
+              </h2>
+              <hr></hr>
+              <p>Note Date: {note.noteDate.substring(0, 10)}</p>
+              <p>Description: {note.description}</p>
+              {note.numberOfFiles > 0 ? (
+                <IconButton
+                  component={Link}
+                  to={{
+                    state: note._id,
+                    pathname: `/view-files`,
+                  }}
+                  className="viewBtn"
+                  data-toggle="tooltip"
+                  title="View Your Saved Files"
+                  onClick={() => viewFolder(note._id)}
+                >
+                  <VisibilityIcon />
+                </IconButton>
+              ) : (
+                <pre></pre>
+              )}
+              {/* </Link> */}
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div>
+          <h2>No folder is created</h2>
+        </div>
+      )}
     </div>
   );
 }
