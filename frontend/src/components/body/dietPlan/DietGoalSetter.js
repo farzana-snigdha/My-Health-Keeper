949c91f0dc3df6932c86a06ed8fb740ae755a643
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2021-07-28 19:00:00 +0600

    diet plan modified

--- frontend/src/components/body/dietPlan/DietGoalSetter.js
+++ frontend/src/components/body/dietPlan/DietGoalSetter.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from "react";
+import { Button, Grid, Link } from "@material-ui/core";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import { NavLink, useHistory, useLocation } from "react-router-dom";
+import "../../../static/Styling/dietPlanGoal.css";
 
 export default function DietGoalSetter() {
-    return (
-        <div>
-           hi 
-        </div>
-    )
+  return (
+    <div>
+      <div className="set_diet_goal_button">
+        <Link
+          data-toggle="tooltip"
+          title="Return to Diet Plan"
+          className="set_diet_goal_button_sub"
+          component={NavLink}
+          to="/diet-plans"
+        >
+          <ArrowBackIcon /> Return
+        </Link>
+      </div>
+    </div>
+  );
 }
--- frontend/src/components/body/dietPlan/DietPlan.js
+++ frontend/src/components/body/dietPlan/DietPlan.js
@@ -1,13 +1,17 @@
 import React, {useState,useEffect} from 'react'
 import { Button,Link } from "@material-ui/core";
 import '../../../static/Styling/dietPlan.css'
+import { NavLink } from 'react-router-dom';
 
 export default function DietPlan() {
     return (
         <div>
-            <Link href="/set-diet-goal" className="diet_buttons">
+            <div className="diet_buttons">
+            <Link component={NavLink} to="/set-diet-goal" className="diet_buttons_sub">
           {""} Set Your Goal
-        </Link> 
+        </Link>
+            </div>
+            
         </div>
     )
 }
