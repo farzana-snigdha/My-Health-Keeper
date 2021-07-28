71e736bfeda89cd103247dc06106618f502e9761
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2021-07-28 21:20:20 +0600

    diet target basic ui added

--- frontend/src/components/body/dietPlan/DietGoalSetter.js
+++ frontend/src/components/body/dietPlan/DietGoalSetter.js
@@ -1,22 +1,132 @@
 import React, { useState, useEffect, useRef } from "react";
-import { Button, Grid, Link } from "@material-ui/core";
+import { Button, Grid, TextField, MenuItem, Select } from "@material-ui/core";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import { NavLink, useHistory, useLocation } from "react-router-dom";
 import "../../../static/Styling/dietPlanGoal.css";
+import { useSelector } from "react-redux";
+import axios from "axios";
+
+const initialState = {
+  height: "",
+  weight: "",
+  age: "",
+  levelOfActivity: "",
+  target: "",
+};
 
 export default function DietGoalSetter() {
+  const token = useSelector((state) => state.token);
+  const auth = useSelector((state) => state.auth);
+  const { user, isLogged } = auth;
+  const [basicInfo, setBasicInfo] = useState(initialState);
+  const { height, weight, age, levelOfActivity, target } = basicInfo;
+
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target;
+    setBasicInfo({ ...basicInfo, [name]: value });
+  };
   return (
-    <div>
+    <div className="root">
+      <Grid className="goal_grid" spacing={3} container>
+        <Grid className="goal_grid_item" item xs={4}>
+          <div
+            data-toggle="tooltip"
+            title="Height in inches"
+            className="goal_font"
+          >
+            Height
+          </div>
+          <TextField
+            fullWidth
+            type="number"
+            id="height"
+            name="height"
+            placeholder="Height in inches"
+            onChange={handleChangeInput}
+            value={height}
+          />
+        </Grid>
+        <Grid className="goal_grid_item" item xs={4}>
+          <div
+            data-toggle="tooltip"
+            title="Weight in kgs"
+            className="goal_font"
+          >
+            Weight
+          </div>
+          <TextField
+            fullWidth
+            type="number"
+            id="weight"
+            name="weight"
+            placeholder="Weight in kgs"
+            onChange={handleChangeInput}
+            value={weight}
+          />
+        </Grid>
+        <Grid className="goal_grid_item" item xs={4}>
+          <div data-toggle="tooltip" title="Age in years" className="goal_font">
+            Age
+          </div>
+          <TextField
+            fullWidth
+            type="number"
+            id="age"
+            name="age"
+            placeholder="Age in years"
+            onChange={handleChangeInput}
+            value={age}
+          />
+        </Grid>
+        <Grid className="goal_grid_item" item xs={7.5}>
+          <div className="goal_font">
+            Level of Activity: &emsp;{" "}
+            <Select
+              id="levelOfActivity"
+              name="levelOfActivity"
+              value={levelOfActivity}
+              onChange={handleChangeInput}
+              autoWidth
+            >
+              <MenuItem value={"sedentary"}>
+                Sedentary (little or no exercise)
+              </MenuItem>
+              <MenuItem value={"lightly active"}>
+                {" "}
+                Lightly active (light exercise/sports 1-3 days/week)
+              </MenuItem>
+              <MenuItem value={"moderately active"}>
+                Moderately active (moderate exercise/sports 3-5 days/week)
+              </MenuItem>
+              <MenuItem value={"very active"}>
+                Very active (hard exercise/sports 6-7 days a week)
+              </MenuItem>
+              <MenuItem value={"extra active"}>
+                Extra active (very hard exercise/sports and physical job or 2x
+                training)
+              </MenuItem>
+            </Select>
+          </div>
+        </Grid>
+        <Grid className="goal_grid_item" item xs={4.5}>
+          <div className="goal_font">
+            Your Target: &emsp;{" "}
+            <Select
+              id="target"
+              name="target"
+              value={target}
+              onChange={handleChangeInput}
+              autoWidth
+            >
+              <MenuItem value={"gain"}>Weight Gain</MenuItem>
+              <MenuItem value={"same"}>Maintain Current Weight</MenuItem>
+              <MenuItem value={"loss"}>Weight Loss</MenuItem>
+            </Select>
+          </div>
+        </Grid>
+      </Grid>
       <div className="set_diet_goal_button">
-        <Link
-          data-toggle="tooltip"
-          title="Return to Diet Plan"
-          className="set_diet_goal_button_sub"
-          component={NavLink}
-          to="/diet-plans"
-        >
-          <ArrowBackIcon /> Return
-        </Link>
+        <Button className="set_diet_goal_button_sub">SAVE</Button>
       </div>
     </div>
   );
--- frontend/src/components/body/dietPlan/DietPlan.js
+++ frontend/src/components/body/dietPlan/DietPlan.js
@@ -2,16 +2,22 @@ import React, {useState,useEffect} from 'react'
 import { Button,Link } from "@material-ui/core";
 import '../../../static/Styling/dietPlan.css'
 import { NavLink } from 'react-router-dom';
+import DietGoalSetter from './DietGoalSetter';
+import { useSelector } from "react-redux";
+import axios from "axios";
+
 
 export default function DietPlan() {
+    const token = useSelector((state) => state.token);
+    
     return (
         <div>
             <div className="diet_buttons">
-            <Link component={NavLink} to="/set-diet-goal" className="diet_buttons_sub">
+            <Button  className="diet_buttons_sub">
           {""} Set Your Goal
-        </Link>
+        </Button>
             </div>
-            
+           <DietGoalSetter/>
         </div>
     )
 }
