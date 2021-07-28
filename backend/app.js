47d3f0eb9cd714ce9b29336cb96c8f6590fc0895
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2021-07-28 22:01:58 +0600

    diet plan routes added

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const cors = require("cors");
-const router = require("./routers/userRouter");
+const authRoutes = require("./routers/authRouter");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const medReminderRouter = require("./routers/medReminder.route");
+const medReminderRoutes = require("./routers/medReminder.route");
 const cycleTracker = require("./routers/cycleTracker.routers");
-const medDoses = require("./routers/medDose.route");
-const payment = require("./routers/payment.routers");
-const specializedHealthInformation = require("./routers/specializedHealthInformation.routers");
+const medDosesRoutes = require("./routers/medDose.route");
+const paymentRoutes = require("./routers/payment.routers");
+const dietPlanRoutes=require('./routers/dietPlan.routers')
+const spHealthInformationRoutes = require("./routers/specializedHealthInformation.routers");
 const path = require("path");
 
 const app = express();
@@ -18,14 +19,14 @@ app.use(cors());
 app.use(cookieParser());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-app.use("/api", specializedHealthInformation);
-app.use(medReminderRouter);
-app.use(medDoses);
-
+app.use("/api", spHealthInformationRoutes);
+app.use(medReminderRoutes);
+app.use(medDosesRoutes);
+app.use('/diet-plan',dietPlanRoutes)
 app.use("/user", cycleTracker);
-app.use("/user", router);
+app.use("/user", authRoutes);
 
-app.use("/payment", payment);
+app.use("/payment", paymentRoutes);
 
 
 module.exports = app;
